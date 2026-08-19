c956e37a3bebc1e72883b42232044599fc05036d
Danish Khan <myselfdanish12@gmail.com>
2026-08-20 00:54:54 +0530

    Update diagonal_sum_mat.cpp

--- diagonal_sum_mat.cpp
+++ diagonal_sum_mat.cpp
@@ -1,7 +1,7 @@
  #include<iostream>
  using namespace std;
 
-int diagonalSum(int mat[][4],int n){
+int diagonalSum(int mat[][3],int n){
     int sum=0;
 
     // for(int i=0 ; i<n ;i++){  //n^2
@@ -9,7 +9,7 @@ int diagonalSum(int mat[][4],int n){
     //         if( i==j ){
     //             sum+= mat[i][j];
     //         }
-    //         else if(j== n-i-1){
+    //         else if(j == n-i-1){
     //             sum+=mat[i][j];
 
     //         }
@@ -21,21 +21,17 @@ int diagonalSum(int mat[][4],int n){
     for(int i=0;i<n;i++){
         sum += mat[i][i]; //pd
         if(i!=n-i-1){
-            sum +=mat[i][n-i-1];
+            sum +=mat[i][n-i-1];  //sd
         }
 
     }
     
-
-
-
-
     cout<< "sum = "<<sum <<endl;
     return sum;
 }
 
  int main(){
-    int matrix[4][4]={{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
+    int matrix[3][3]={{1,2,3},{5,6,7},{9,10,11}};
 
-    diagonalSum(matrix,4);
+    diagonalSum(matrix,3);
  }
